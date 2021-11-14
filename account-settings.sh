b291a367f62027d877eda18e291ca6ca5c7661b9
James Fulford <james.fulford@outlook.com>
2021-11-13 21:49:00 -0500

    set some account settings

---
+++ account-settings.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+suspend_trade="$1"
+if [[ "false" != "$suspend_trade" ]]; then
+    suspend_trade="false"
+fi
+
+echo "suspend_trade: $suspend_trade"
+
+curl --silent -X PATCH \
+    -H "APCA-API-KEY-ID: $APCA_API_KEY_ID" \
+    -H "APCA-API-SECRET-KEY: $APCA_API_SECRET_KEY" \
+    ${ALPACA_URL}/v2/account/configurations \
+    -d "{
+        \"dtbp_check\": \"both\",
+        \"max_margin_multiplier\": 1,
+        \"fractional_trading\": false,
+        \"no_shorting\": true,
+        \"suspend_trade\": $suspend_trade,
+        \"trade_confirm_email\": \"all\"
+    }" | python -m json.tool
