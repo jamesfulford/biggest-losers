ff0e1d3ded6f5f7ebf030d52abe4d54b85b7ae72
James Fulford <james.fulford@outlook.com>
2021-11-13 21:31:50 -0500

    starting to play with alpaca API

---
+++ buy-eth.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl --silent -X POST \
+        -H "APCA-API-KEY-ID: $APCA_API_KEY_ID" \
+        -H "APCA-API-SECRET-KEY: $APCA_API_SECRET_KEY" \
+        ${ALPACA_URL}/v2/orders \
+        -d "{
+            \"symbol\": \"ETHUSD\",
+            \"notional\": \"1000\",
+            \"side\": \"buy\",
+            \"type\": \"market\",
+            \"time_in_force\": \"gtc\"
+        }" | python -m json.tool
---
+++ clear-account.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function alpaca () {
+    local verb="$1"
+    local url_path="$2"
+
+    echo "$verb $url_path"
+    shift 2
+
+    curl --silent -X $verb \
+        -H "APCA-API-KEY-ID: $APCA_API_KEY_ID" \
+        -H "APCA-API-SECRET-KEY: $APCA_API_SECRET_KEY" \
+        ${ALPACA_URL}${url_path} \
+        "$@" | python -m json.tool
+    echo
+}
+
+
+alpaca GET /v1/account
+alpaca GET /v1/positions
+alpaca GET /v1/orders
+
+alpaca DELETE /v2/orders
+alpaca DELETE /v2/positions
+
+alpaca GET /v1/account
+alpaca GET /v1/positions
+alpaca GET /v1/orders
