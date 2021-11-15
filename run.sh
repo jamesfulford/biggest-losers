d1f21496bc672a018dc59551a4355f6cf85ba9c5
James Fulford <james.fulford@outlook.com>
2021-11-15 09:16:28 -0500

    bit nicer logs

--- run.sh
+++ run.sh
@@ -3,15 +3,24 @@
 # in crontab:
 # */10 * * * 1-5 cd /Users/jamesfulford/scanners && ./run.sh >> /tmp/run.log 2>&1
 
+echo
+echo "###"
+echo "### starting new run in run.sh"
+echo "###"
+echo
+date
+echo
+
 . paper.env ; /usr/local/bin/python3 run.py
+echo "(return code was $?)"
 
 H=$(date +%H)
 if (( 12 <= 10#$H && 10#$H < 13 )); then 
-    echo midday, rotating logs
     if [[ -f /tmp/run.log.$(date +%Y-%m-%d) ]]; then
-        echo "found log file, won't rotate"
+        echo "# already found today's log file, won't rotate"
         exit 0
     fi
+    echo "# rotating logs"
     cp -f /tmp/run.log /tmp/run.log.$(date +%Y-%m-%d)
-    echo starting new log
+    echo "# starting new log"
 fi
