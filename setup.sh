24cd5024b012e711f06f1c2859fb0b0a6e958204
James Fulford <james.fulford@outlook.com>
2021-11-22 15:54:07 -0500

    crontab todo

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ which pip3 || fail_script "could not find 'pip3'"
 
 pip3 install -r requirements.txt || fail_script "failed to install requirements.txt"
 
+# TODO: account for timezones in crontab entries
 crontab -l | grep -q "45 15 \* \* \* cd ~/biggest-losers && \./run\.sh >> /tmp/run\.log 2>&1" || fail_script "could not find buy cron job"
 crontab -l | grep -q "0 20 \* \* \* cd ~/biggest-losers && \./run\.sh >> /tmp/run\.log 2>&1" || fail_script "could not find sell cron job"
 test -f ~/biggest-losers/run.sh || fail_script "could not find run.sh referenced by crontab"
