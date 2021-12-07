e0980667488eb22900a09852d5b2346c44a6e9fc
James Fulford <james.fulford@outlook.com>
2021-12-07 14:48:18 -0500

    fetching orders with pagination

--- src/broker.py
+++ src/broker.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from datetime import date, datetime, timedelta
 
 
 ALPACA_URL = os.environ['ALPACA_URL']
@@ -14,6 +15,12 @@ APCA_HEADERS = {
 DRY_RUN = 'DRY_RUN' in os.environ
 if DRY_RUN:
     print('DRY RUN, will not execute any trades')
+
+
+def _get_alpaca(url):
+    response = requests.get(ALPACA_URL + url, headers=APCA_HEADERS)
+    response.raise_for_status()
+    return response.json()
 
 
 def buy_symbol_at_close(symbol, quantity):
@@ -48,14 +55,61 @@ def liquidate():
 
 
 def get_positions():
-    response = requests.get(
-        ALPACA_URL + '/v2/positions', headers=APCA_HEADERS)
-    response.raise_for_status()
-    return response.json()
+    return _get_alpaca('/v2/positions')
 
 
 def get_account():
-    response = requests.get(
-        ALPACA_URL + '/v1/account', headers=APCA_HEADERS)
-    response.raise_for_status()
-    return response.json()
+    return _get_alpaca('/v1/account')
+
+
+def get_open_orders():
+    return _get_alpaca('/v1/orders')
+
+
+def get_filled_orders(start: date, end: date):
+    # inclusive (API is exclusive)
+    # sorted from earliest to latest
+
+    limit = 500
+
+    def _fetch_closed_orders(after: datetime, until: datetime):
+        print(f"fetching up to {limit} closed orders after {after}")
+        results = _get_alpaca(
+            f'/v2/orders?status=closed&after={after}&until={until}&direction=asc&limit={limit}')
+
+        if len(results) == limit:
+            # because asc, [-1] is latest
+            # subtract 1s so we don't miss any orders. We have to deduplicate them later.
+            latest_submitted_at = datetime.strptime(
+                results[-1]["submitted_at"], "%Y-%m-%dT%H:%M:%S.%fZ") - timedelta(seconds=1)
+
+            if latest_submitted_at <= after:
+                # this will happen if {limit} orders are submitted in the same second
+                raise Exception(
+                    "too many results in same 1s period {latest_submitted_at}")
+
+            new_results = _fetch_closed_orders(latest_submitted_at, until)
+
+            return results + new_results
+
+        return results
+
+    results = _fetch_closed_orders(
+        start - timedelta(seconds=1), end + timedelta(seconds=1))
+
+    # deduplicate
+    id_set = set()
+    deduped_results = []
+    for result in results:
+        if result["id"] not in id_set:
+            deduped_results.append(result)
+            id_set.add(result["id"])
+
+    # filled orders are sorted from earliest to latest
+    deduped_results = list(
+        filter(lambda x: x["status"] == 'filled', deduped_results))
+
+    # should already be sorted, but put this here to be safe
+    deduped_results.sort(key=lambda x: x["submitted_at"])
+
+    return deduped_results
