daea98a1d31bd19fbda1222b379b1586ad80ba15
James Fulford <james.fulford@outlook.com>
2022-01-04 10:32:30 -0500

    fix(scripts): actually sync back logs

--- scripts/deploy/sync-data-back.sh
+++ scripts/deploy/sync-data-back.sh
@@ -5,4 +5,4 @@ server="solomon"
 
 
 mkdir -p ~/biggest-losers-data/remote-environments/$environment/
-rsync -razv $server:~/$environment-data/ ~/biggest-losers-data/remote-environments/$environment/
+rsync --delete -razv $server:~/$environment-data/ ~/biggest-losers-data/remote-environments/$environment/
