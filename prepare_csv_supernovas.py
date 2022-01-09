0eef01a6677e983d5b0d68e566b522df76915c5a
James Fulford <james.fulford@outlook.com>
2022-01-09 15:56:46 -0500

    feat(supernovas): today close

--- prepare_csv_supernovas.py
+++ prepare_csv_supernovas.py
@@ -17,9 +17,12 @@ def get_all_supernovas(start: date, end: date):
             continue
 
         for nova in today_supernovas:
-            if nova['mover_day_of_action']['percent_change_high'] > 1000:
-                print("WARNING: supernova is too big:",
-                      nova['mover_day_of_action']['T'], nova['day_of_action'])
+            if nova["mover_day_of_action"]["percent_change_high"] > 1000:
+                print(
+                    "WARNING: supernova is too big:",
+                    nova["mover_day_of_action"]["T"],
+                    nova["day_of_action"],
+                )
                 continue
             novas.append(nova)
 
@@ -34,27 +37,29 @@ csv_headers = [
     "yesterday_close",
     "today_high",
     "yesterday_volume",
-    "today_volume"
+    "today_volume",
 ]
 
 
 def prepare_supernovas_csv(path: str, start, end):
-    novas = get_all_supernovas(
-        start, end)
+    novas = get_all_supernovas(start, end)
 
     def yield_supernovas():
         for nova in novas:
             # print(nova)
             # print(nova['mover_day_after'])
             yield {
-                "ticker": nova['mover_day_of_action']['T'],
-                "is_warrant": is_warrant(nova['mover_day_of_action']['T']),
-                "day_of_action": nova['day_of_action'],
-                "percent_change_high": nova['mover_day_of_action']['percent_change_high'],
-                "yesterday_close": nova['mover_day_of_action']['previous_day_close'],
-                "today_high": nova['mover_day_of_action']['h'],
-                "yesterday_volume": nova['mover_day_of_action']['previous_day_volume'],
-                "today_volume": nova['mover_day_of_action']['v']
+                "ticker": nova["mover_day_of_action"]["T"],
+                "is_warrant": is_warrant(nova["mover_day_of_action"]["T"]),
+                "day_of_action": nova["day_of_action"],
+                "percent_change_high": nova["mover_day_of_action"][
+                    "percent_change_high"
+                ],
+                "yesterday_close": nova["mover_day_of_action"]["previous_day_close"],
+                "today_high": nova["mover_day_of_action"]["h"],
+                "yesterday_volume": nova["mover_day_of_action"]["previous_day_volume"],
+                "today_volume": nova["mover_day_of_action"]["v"],
+                "today_close": nova["mover_day_of_action"]["c"],
             }
 
     write_csv(path, yield_supernovas(), csv_headers)
@@ -62,14 +67,14 @@ def prepare_supernovas_csv(path: str, start, end):
 
 def main():
     from src.pathing import get_paths
-    path = get_paths()['data']['outputs']["supernovas_csv"]
+
+    path = get_paths()["data"]["outputs"]["supernovas_csv"]
 
     start, end = get_cache_prepared_date_range_with_leadup_days(1)
 
     print("start:", start)
     print("end:", end)
-    print("estimated trading days:", len(
-        list(generate_trading_days(start, end))))
+    print("estimated trading days:", len(list(generate_trading_days(start, end))))
 
     prepare_supernovas_csv(path, start, end)
 
