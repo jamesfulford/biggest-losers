baa46ec3e08d0f986efbd7812933b0714e74a4cd
James Fulford <james.fulford@outlook.com>
2022-01-11 12:49:54 -0500

    feat(dump-orders): only dump for 2022

--- dump-orders.py
+++ dump-orders.py
@@ -12,21 +12,23 @@ target_environment_name = sys.argv[1] if len(sys.argv) > 2 else None
 MARKET_TZ = ZoneInfo("America/New_York")
 
 # TODO: read from config so intrac1 doesn't take so long during deploys
-start = datetime(2000, 1, 1)
+start = datetime(2022, 1, 1)
 end = datetime.now() + timedelta(days=1)
 
 filled_orders = get_filled_orders(start, end)
 
-path = get_paths(target_environment_name)[
-    'data']["outputs"]["filled_orders_csv"]
-with open(path, 'w') as f:
+path = get_paths(target_environment_name)["data"]["outputs"]["filled_orders_csv"]
+with open(path, "w") as f:
     f.write("Date,Time,Symbol,Quantity,Price,Side\n")
     for order in filled_orders:
-        now = datetime.strptime(
-            order['filled_at'], "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc).astimezone(MARKET_TZ)
-        ticker = order['symbol']
-        quantity = order['filled_qty']
-        price = order['filled_avg_price']
-        side = order['side']
+        now = (
+            datetime.strptime(order["filled_at"], "%Y-%m-%dT%H:%M:%S.%fZ")
+            .replace(tzinfo=timezone.utc)
+            .astimezone(MARKET_TZ)
+        )
+        ticker = order["symbol"]
+        quantity = order["filled_qty"]
+        price = order["filled_avg_price"]
+        side = order["side"]
         s = f"{now.strftime('%Y-%m-%d')},{now.strftime('%H:%M:%S')},{ticker},{quantity},{price},{side.upper()}\n"
         f.write(s)
