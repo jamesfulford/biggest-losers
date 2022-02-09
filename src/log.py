093f05092d9654123aa4736f8d8dbcd87a0bacde
James Fulford <james.fulford@outlook.com>
2022-02-09 10:20:23 -0500

    fix(log): stdout, not stderr

--- src/log.py
+++ src/log.py
@@ -1,4 +1,6 @@
 import logging
+import sys
 
 logging.basicConfig(level=logging.INFO,
-                    format="%(asctime)-25s %(levelname)-8s %(message)s")
+                    format="%(asctime)-25s %(levelname)-8s %(message)s",
+                    handlers=[logging.StreamHandler(sys.stdout)])
