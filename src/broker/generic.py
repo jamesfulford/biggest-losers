790227b5a06dc2cca70ac3359765a693ef2bd960
JE Fulford <jefulford@outlook.com>
2022-02-28 13:42:18 -0500

    added pizzalabs (placeholder broker)

--- src/broker/generic.py
+++ src/broker/generic.py
@@ -22,6 +22,15 @@ elif broker_name == "td":
     get_positions = td.get_positions
     get_account = td.get_account
     get_filled_orders = td.get_filled_orders
+elif broker_name == "pizzalabs":
+    import src.broker.pizzalabs as pizzalabs
+    buy_symbol_at_close = pizzalabs.buy_symbol_at_close
+    buy_symbol_market = pizzalabs.buy_symbol_market
+    sell_symbol_market = pizzalabs.sell_symbol_market
+    sell_symbol_at_open = pizzalabs.sell_symbol_at_open
+    get_positions = pizzalabs.get_positions
+    get_account = pizzalabs.get_account
+    get_filled_orders = pizzalabs.get_filled_orders
 else:
     logging.fatal(f"BROKER '{broker_name}' not supported. Exiting...")
     exit(1)
---
+++ src/broker/pizzalabs.py
@@ -0,0 +1,37 @@
+import logging
+logging.warn(
+    "YOU'RE IN PIZZA LABS NOW! YOU CAN CHECK OUT, BUT YOU CAN NEVER LEAVE!")
+
+
+def buy_symbol_at_close(*args, **kwargs):
+    logging.info(f"buy_symbol_at_close: {args=} {kwargs=}")
+
+
+def buy_symbol_market(*args, **kwargs):
+    logging.info(f"buy_symbol_market: {args=} {kwargs=}")
+
+
+def sell_symbol_market(*args, **kwargs):
+    logging.info(f"sell_symbol_market: {args=} {kwargs=}")
+
+
+def sell_symbol_at_open(*args, **kwargs):
+    logging.info(f"sell_symbol_at_open: {args=} {kwargs=}")
+
+
+def get_positions(*args, **kwargs):
+    logging.info(f"get_positions: {args=} {kwargs=}")
+    return []
+
+
+def get_account(*args, **kwargs):
+    logging.info(f"get_account: {args=} {kwargs=}")
+    return {
+        "type": "MARGIN",
+        "equity": 1337000,
+        "cash": 1337000,
+    }
+
+
+def get_filled_orders(*args, **kwargs):
+    logging.info(f"get_filled_orders: {args=} {kwargs=}")
