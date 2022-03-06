ca0fe96e3bb1e7436845b31e4f248178b6d47d53
James Fulford <james.fulford@outlook.com>
2022-03-06 13:35:14 -0500

    cleaning up dev dir

--- dev/broker.sh
+++ dev/broker.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-./run.sh refresh-tokens
-. cash1.env && . paper.env && nodemon -e py -x "DRY_RUN=1 python3 -c 'import src.log; from src.broker.generic import main; main()'"
+ENV_NAME=${ENV_NAME:-"paper"}
+
+function dev_source_env() {
+    ENV_NAME=${ENV_NAME:-"paper"}
+    if [[ ! -f $ENV_NAME.env ]]; then
+        echo "Could not find $ENV_NAME.env, exiting"
+        exit 1
+    fi
+    cat $ENV_NAME.env | sed "s/..*/export &/" > active.dev.env
+    source active.dev.env
+}
+
+ENV_NAME=paper dev_source_env
+ENV_NAME=cash1 dev_source_env
+ENV_FILE=cash1.env ./run.sh refresh-tokens
+
+nodemon -e py -x "DRY_RUN=1 python3 -c 'import src.log; from src.broker.generic import main; main()'"
--- dev/polygon.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-. paper.env && nodemon -e py -x "python3 -c 'import src.log; from src.get_candles import main; main()'"
