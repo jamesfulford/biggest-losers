92a97fbbef672a7d46cb54f0f0c0856d03ec6efd
James Fulford <james.fulford@outlook.com>
2022-03-09 00:27:59 -0500

    feat(meemaw): clear-account logs

--- src/strat/meemaw/clear_account.py
+++ src/strat/meemaw/clear_account.py
@@ -1,12 +1,17 @@
+import logging
 from src.broker.generic import cancel_all_orders, get_positions, sell_symbol_market
 
 
 def main():
+    logging.info("Cancelling all orders...")
     cancel_all_orders()
-    positions = get_positions()
 
+    logging.info("Liquidating all positions...")
+    positions = get_positions()
     for position in positions:
         sell_symbol_market(position["symbol"], position["qty"])
+
+    logging.info("Done.")
 
 
 if __name__ == "__main__":
