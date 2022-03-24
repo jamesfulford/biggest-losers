479486b1e6801bfe31e3a3cdf6c097c799e51159
James Fulford <james.fulford@outlook.com>
2022-03-23 22:02:50 -0400

    fix(volume_movers): adjust to use latest scanner arch

--- src/scan/volume_movers.py
+++ src/scan/volume_movers.py
@@ -1,43 +1,41 @@
 from datetime import date
-import logging
+from typing import cast
 
-from src.criteria import is_etf, is_right, is_stock, is_unit, is_warrant
+from src.criteria import is_etf, is_stock
+from src.data.polygon.grouped_aggs import Ticker
 from src.scan.utils.all_tickers_on_day import get_all_tickers_on_day
 from src.scan.utils.asset_class import enrich_tickers_with_asset_class
 from src.scan.utils.indicators import enrich_tickers_with_indicators, from_yesterday_candle
 from src.scan.utils.rank import rank_candidates_by
-from src.trading_day import generate_trading_days
-from src.data.polygon.grouped_aggs import get_cache_prepared_date_range_with_leadup_days
-from src.csv_dump import write_csv
+from src.strat.utils.prescanner import build_prescanner_with_empty_candle_getter
+from src.strat.utils.scanners import CandleGetter, ScannerFilter
 
 
-#
-# _on_day: used for LIVE and BACKTEST
-# - all filtering logic should be here
-# - all critical indicators should be enriched in here
-#
-# Some tips:
-# - try to filter on OHLCV first before getting daily candles or calculating indicators
-def get_all_candidates_on_day(today: date, skip_cache=False):
+LEADUP_PERIOD = 1
+
+
+class Candidate(Ticker):
+    yesterday_v: float
+    volume_percent_change: float
+
+
+def scanner(provided_tickers: list[Ticker], today: date, _candle_getter: CandleGetter) -> list[Candidate]:
     """
     NOTE: this scanner would be hard to use intraday because volume is not complete
     """
-    tickers = get_all_tickers_on_day(today, skip_cache=skip_cache)
-    tickers = list(filter(lambda t: t["v"] > 100000, tickers))
+    tickers = cast(list[Candidate], provided_tickers)
+    tickers = list(filter(lambda t: t["v"] > 100_000, tickers))
 
     tickers = list(enrich_tickers_with_asset_class(today, tickers, {
         "is_etf": is_etf,
-        "is_right": is_right,
         "is_stock": is_stock,
-        "is_unit": is_unit,
-        "is_warrant": is_warrant,
     }))
 
     tickers = list(enrich_tickers_with_indicators(today, tickers, {
         "yesterday_v": from_yesterday_candle("v"),
-    }, n=2))
+    }, n=LEADUP_PERIOD + 1))
 
-    tickers = list(filter(lambda t: t["yesterday_v"] > 100000, tickers))
+    tickers = list(filter(lambda t: t["yesterday_v"] > 100_000, tickers))
 
     for ticker in tickers:
         ticker['volume_percent_change'] = (
@@ -51,73 +49,7 @@ def get_all_candidates_on_day(today: date, skip_cache=False):
     return tickers
 
 
-def get_all_candidates_between_days(start: date, end: date):
-    for day in generate_trading_days(start, end):
-        for candidate in get_all_candidates_on_day(day) or []:
-            candidate["day_of_action"] = day
-            yield candidate
-
-
-def build_row(candidate: dict):
-    return {
-        "day_of_action": candidate['day_of_action'],
-        # ticker insights
-        "T": candidate['T'],
-        "is_stock": candidate['is_stock'],
-        "is_etf": candidate['is_etf'],
-        "is_warrant": candidate['is_warrant'],
-        "is_unit": candidate['is_unit'],
-        "is_right": candidate['is_right'],
-
-        # day_of_action stats
-        "o": candidate["o"],
-        "h": candidate["h"],
-        "l": candidate["l"],
-        "c": candidate["c"],
-        "v": candidate["v"],
-        "n": candidate["n"],
-
-        # rank
-        "rank": candidate["rank"],
-
-        # indicators
-        "vw": candidate["vw"],
-        "yesterday_v": candidate["yesterday_v"],
-        "volume_percent_change": candidate["volume_percent_change"],
-    }
-
-
-def prepare_biggest_losers_csv(path: str, start: date, end: date):
-    write_csv(
-        path,
-        map(build_row, get_all_candidates_between_days(start, end)),
-        headers=[
-            "day_of_action",
-            "T",
-            "is_stock",
-            "is_etf",
-            "is_warrant",
-            "is_unit",
-            "is_right",
-            "o",
-            "h",
-            "l",
-            "c",
-            "v",
-        ]
-    )
-
-
-def prepare_csv():
-    from src.pathing import get_paths
-
-    path = get_paths()["data"]["outputs"]["volume_movers_csv"]
-
-    start, end = get_cache_prepared_date_range_with_leadup_days(0)
-
-    logging.info(f"start: {start}")
-    logging.info(f"end: {end}")
-    logging.info(
-        f"estimated trading days: {len(list(generate_trading_days(start, end)))}")
-
-    prepare_biggest_losers_csv(path, start=start, end=end)
+prescanner = build_prescanner_with_empty_candle_getter(
+    # cast: list[Candidate] -> list[Ticker], mypy/Python doesn't understand that `Candidate` is a subtype of `Ticker`
+    cast(ScannerFilter, scanner)
+)
