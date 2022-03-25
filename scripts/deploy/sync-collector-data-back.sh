27c93100c7dc552c58c549a31850556ea3316006
James Fulford <james.fulford@outlook.com>
2022-03-25 00:10:34 -0400

    feat(sync): sync local cache (safely) to server

--- scripts/deploy/sync-collector-data-back.sh
+++ scripts/deploy/sync-collector-data-back.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -e
 
+function sync_local_data_dir_subdir_safe() {
+  local subdir=$1
+
+  until rsync -razv ~/biggest-losers-data/$subdir/ solomon:~/collector-data/$subdir;
+  do
+    echo "rsync failed, retrying in 30 seconds..."
+    sleep 30
+  done
+}
+
 function sync_data_dir_subdir() {
   local subdir=$1
 
@@ -22,5 +32,6 @@ function safe_sync_data_dir_subdir() {
   done
 }
 
+sync_local_data_dir_subdir_safe cache
 sync_data_dir_subdir cache
 safe_sync_data_dir_subdir chronicles
