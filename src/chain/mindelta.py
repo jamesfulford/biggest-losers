6a61d1d807a9b9a75f4601ef3f7b5d8a4392ec88
James Fulford <james.fulford@outlook.com>
2022-05-12 16:58:33 -0400

    feat(chain): pick with delta

--- src/chain/mindelta.py
+++ src/chain/mindelta.py
@@ -3,7 +3,7 @@ import mibian
 
 import datetime
 import logging
-from typing import Iterator, Optional
+from typing import Iterator, Optional, Tuple
 import typing
 from src.chain.gramma import OptionSimulation
 from src.chain.utils import filter_option_chain_for_calls, filter_option_chain_for_expiration, filter_option_chain_for_near_the_money, filter_option_chain_for_normality, filter_option_chain_for_out_of_the_money, filter_option_chain_for_puts
@@ -18,11 +18,21 @@ from src.data.types.contracts import OptionCandleGetter, OptionContractSpecifier
 from src.trading_day import now
 
 
+def yield_contracts_toward_oom(contracts: list[PolygonOptionChainContract]) -> Iterator[PolygonOptionChainContract]:
+    yield from sorted(contracts, key=lambda c: c['spec']['strike_price'], reverse=contracts[0]['spec']['contract_type'] == 'put')
+
+
+def yield_contracts_from_soonest(contracts: list[PolygonOptionChainContract]) -> Iterator[Tuple[datetime.date, list[PolygonOptionChainContract]]]:
+    contracts_by_expiration = {}
+    for contract in contracts:
+        contracts_by_expiration.setdefault(
+            contract['spec']['expiration_date'], []).append(contract)
+
+    for expiration_date, contracts in sorted(contracts_by_expiration.items()):
+        yield expiration_date, contracts
+
+
 def pick_favorite_contracts(contracts: list[PolygonOptionChainContract], current_price: float, candle_getter: OptionCandleGetter, option_price_range: tuple[int, int] = (1, 5)) -> Iterator[PolygonOptionChainContract]:
-    """
-    Picks normal contracts expiring soon (but not too soon) that are out of the money but close.
-    This should find contracts with high gammas. TODO: confirm
-    """
     contracts = filter_option_chain_for_normality(contracts)
     contracts = filter_option_chain_for_expiration(
         contracts, min_days_to_expiration=3, max_days_to_expiration=15)
@@ -31,17 +41,13 @@ def pick_favorite_contracts(contracts: list[PolygonOptionChainContract], current
     contracts = filter_option_chain_for_near_the_money(
         contracts, current_price, buffer=10)
 
-    contracts_by_expiration = {}
-    for contract in contracts:
-        contracts_by_expiration.setdefault(
-            contract['spec']['expiration_date'], []).append(contract)
-
     min_option_price, max_option_price = option_price_range
-    for _expiration_date, contracts in sorted(contracts_by_expiration.items()):
-        for contract in sorted(contracts, key=lambda c: c['spec']['strike_price'], reverse=contracts[0]['spec']['contract_type'] == 'put'):
+
+    for expiration_day, contracts in yield_contracts_from_soonest(contracts):
+
+        for contract in yield_contracts_toward_oom(contracts):
             ticker = format_contract_specifier_to_polygon_option_ticker(
                 contract['spec'])
-
             candles = candle_getter(contract['spec'], '1',
                                     contract['chain_as_of_date'], contract['chain_as_of_date'])
             if not candles:
@@ -52,38 +58,37 @@ def pick_favorite_contracts(contracts: list[PolygonOptionChainContract], current
 
             if close > max_option_price:
                 logging.debug(
-                    f'{ticker} is too high {close}, looking at next expiration date')
+                    f'{ticker} premium is too high {close}, looking at next expiration date')
                 break
 
             if close < min_option_price:
-                logging.debug(f'{ticker} is too low {close}')
+                logging.debug(f'{ticker} premium is too low {close}')
                 continue
 
             if volume < 1000:
                 logging.debug(f'{ticker} volume is too low {volume}')
                 continue
 
-            interest_rate = 1
-            annual_dividends = 0.92
+            # Does not matter very much for our timescale, so hardcoding
+            # Using 1 year treasury rate ("risk free" rate of growth): https://www.investopedia.com/articles/active-trading/051415/how-why-interest-rates-affect-options.asp
+            # https://ycharts.com/indicators/1_year_treasury_rate
+            interest_rate = 2
 
-            values = (current_price, contract['spec']['strike_price'], interest_rate,
-                      annual_dividends, contract['days_to_expiration'])
+            values = (current_price, contract['spec']['strike_price'],
+                      interest_rate, contract['days_to_expiration'])
+            c = mibian.BS(values, callPrice=close)
+            c = mibian.BS(values, volatility=c.impliedVolatility)
+            delta = c.callDelta if contract['spec']['contract_type'] == 'call' else c.putDelta
 
-            # print(values, close)
-
-            c = mibian.Me(values, callPrice=close)
-            c = mibian.Me(values, volatility=c.impliedVolatility)
-            delta = c.callDelta
-
-            if delta < 0.5:
+            if abs(delta) < 0.5:
                 logging.debug(f'{ticker} delta is too low {delta}')
                 continue
-            # print(c.callDelta)
 
             logging.debug(f'{ticker} we like. {volume=} {close=}')
             yield contract
 
 
+# TODO: deduplicate from gramma.py
 def simulate_trade_in_options(underlying_symbol: str, start: datetime.datetime, end: datetime.datetime, upside: bool) -> Optional[OptionSimulation]:
     # Get necessary data
     # TODO: start and end may not be same day
@@ -107,13 +112,13 @@ def simulate_trade_in_options(underlying_symbol: str, start: datetime.datetime, 
         contracts = filter_option_chain_for_puts(contracts)
     contract = next(pick_favorite_contracts(
         contracts, current_price, get_truncated_option_candles), None)
+    print(contract)
     if not contract:
         logging.debug("No contract found")
         return
 
     option_candles = get_option_candles(contract['spec'], '1', day, day)
 
-    return
     entry_candle = next(c for c in reversed(
         option_candles) if c['datetime'] <= start)
     exit_candle = next(c for c in option_candles if c['datetime'] >= end)
@@ -144,5 +149,5 @@ def simulate_trade_in_options(underlying_symbol: str, start: datetime.datetime, 
 
 
 def main():
-    simulate_trade_in_options(
-        'AAPL', now() - datetime.timedelta(days=1, minutes=200), now() - datetime.timedelta(days=1, minutes=15), True)
+    print(simulate_trade_in_options(
+        'AAPL', now() - datetime.timedelta(days=1, minutes=200), now() - datetime.timedelta(days=1, minutes=15), True))
