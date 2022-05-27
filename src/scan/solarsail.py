6db6c66ca849766860d4ced649398c37e853ab3d
James Fulford <james.fulford@outlook.com>
2022-05-27 10:57:14 -0400

    feat(solarsail): supernovas but open-close not close-close

--- src/scan/solarsail.py
+++ src/scan/solarsail.py
@@ -1,40 +1,44 @@
-from datetime import date
-from typing import Callable
+import datetime
+import typing
+from src.backtest.chronicle.prescanner import build_prescanner_with_empty_candle_getter, with_high_bias_prescan_strategy
 
 from src.data.polygon.asset_class import is_stock
-from src.data.finnhub.finnhub import get_candles
-from src.scan.utils.all_tickers_on_day import get_all_tickers_on_day
+from src.data.polygon.grouped_aggs import Ticker
 from src.scan.utils.asset_class import enrich_tickers_with_asset_class
-from src.scan.utils.indicators import enrich_tickers_with_indicators, extract_from_n_candles_ago
 from src.scan.utils.rank import rank_candidates_by
+from src.scan.utils.scanners import CandleGetter, ScannerFilter
+
+LEADUP_PERIOD = 0
 
 
-LEADUP_PERIOD = 1
+class Candidate(Ticker):
+    is_stock: bool
+    open_to_close_change: float
 
 
-def get_all_candidates_on_day(today: date, skip_cache=False):
-    tickers = get_all_tickers_on_day(today, skip_cache=skip_cache)
-    return filter_all_candidates_on_day(tickers, today)
-
-
-def filter_all_candidates_on_day(tickers: list, today: date, _candle_getter: Callable = get_candles):
+def scanner(provided_tickers: list[Ticker], today: datetime.date, _candle_getter: CandleGetter) -> list[Candidate]:
+    tickers = typing.cast(list[Candidate], provided_tickers)
     tickers = list(
         filter(lambda t: t["v"] > 100_000, tickers))
 
     tickers = list(enrich_tickers_with_asset_class(today, tickers, {
         "is_stock": is_stock,
     }))
-    tickers = list(enrich_tickers_with_indicators(today, tickers, {
-        "yesterday_close": extract_from_n_candles_ago("c", 1),
-    }, n=LEADUP_PERIOD + 1))
     for ticker in tickers:
-        percent_change_yesterday = (ticker["c"]-ticker["yesterday_close"]
-                                    )/ticker["yesterday_close"]
-        ticker["percent_change_yesterday"] = percent_change_yesterday
+        ticker['open_to_close_change'] = (
+            ticker['c'] - ticker['o']) / ticker['o']
     tickers = list(
-        filter(lambda t: t["percent_change_yesterday"] > 2, tickers))
+        filter(lambda t: t["open_to_close_change"] >= 0.5, tickers))
 
     tickers = rank_candidates_by(
-        tickers, lambda t: -t['percent_change_yesterday'])
+        tickers, lambda t: -t['open_to_close_change'])
 
     return tickers
+
+
+prescanner = with_high_bias_prescan_strategy(
+    build_prescanner_with_empty_candle_getter(
+        # cast: list[Candidate] -> list[Ticker], mypy/Python doesn't understand that `Candidate` is a subtype of `Ticker`
+        typing.cast(ScannerFilter, scanner)
+    ),
+)
